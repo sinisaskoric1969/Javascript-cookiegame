80671c304ceffd9c194da83a94c68169f918f322
sinisaskoric1969 <sinisa.skoric1969@outlook.com>
2020-12-21 15:19:12 +0300

    player move

--- index.js
+++ index.js
@@ -120,3 +120,141 @@ function showwin() {
     objbonbon.style.visibility="hidden";
 
 }
+
+
+// -------------- instruction button-----------------
+bRulesStart.addEventListener("click", showRules);
+function showRules () {
+    theRulesWindow.style.visibility = "visible";
+    theStartWindow.style.visibility= "hidden";
+    bgamewindow.style.visibility = "hidden";
+}
+
+// -------------- back rule button in instruction page -----------------
+bBackRules.addEventListener("click",showstartpage );
+function showstartpage () {
+    bgamewindow.style.visibility = "hidden";
+    theRulesWindow.style.visibility = "hidden";
+    theStartWindow.style.visibility = "visible";
+    loseWindows.style.visibility="hidden";
+    winWindows.style.visibility="hidden";
+}
+
+//------------ button in lose page------------------
+startBack1.addEventListener("click",showstartpage );
+playLose1.addEventListener("click",PlayStart );
+
+
+//------------ button in win page ------------------
+startBack2.addEventListener("click",showstartpage );
+playLose2.addEventListener("click",PlayStart );
+
+
+
+
+
+
+
+
+//--------------------Move player---------------------
+let objplayer= null;
+let oborangefrosting= null;
+let objpinkfrost= null;
+let objtealfrost = null;
+let objchocake = null;
+let objvanillacake = null;
+let objroll = null;
+let objeclair = null;
+let objstars = null
+let objstars1 = null
+let objcherries = null
+let Objmacaron = null;
+let objbonbon = null;
+
+let objSelect = null;
+
+function init(){
+    objplayer=document.getElementById("player");
+    oborangefrosting=document.getElementById("orangefrosting");
+    objpinkfrost=document.getElementById("pinkfrosting");
+    objtealfrost=document.getElementById("tealfrosting");
+    objchocake=document.getElementById("chocolatecake");
+    objvanillacake=document.getElementById("vanillacake");
+    objroll=document.getElementById("cinnamonroll");
+    objeclair=document.getElementById("eclair");
+    objstars=document.getElementById("stars");
+    objstars1=document.getElementById("star1");
+    objcherries=document.getElementById("cherries");
+    objcherrie1=document.getElementById("cherry1");
+    Objmacaron=document.getElementById("macaron");
+    objbonbon=document.getElementById("bonbon");
+
+
+    objplayer.style.position='relative';
+    objplayer.style.left='270px';
+    objplayer.style.top='240px';
+    objplayer.style.visibility="hidden";
+    orderslist.style.visibility="hidden";
+    pastriesmateriallist.style.visibility="hidden";
+    countdownobj.style.visibility="hidden";
+    timerBox.style.visibility="hidden";
+
+
+    oborangefrosting.style.position='relative';
+    oborangefrosting.style.left='0px';
+    oborangefrosting.style.top='15px';
+    oborangefrosting.style.visibility="hidden";
+
+    objpinkfrost.style.position='relative';
+    objpinkfrost.style.left='0px';
+    objpinkfrost.style.top='-15px';
+    objpinkfrost.style.visibility="hidden";
+
+    objtealfrost.style.position='relative';
+    objtealfrost.style.left='-170px';
+    objtealfrost.style.top='10px';
+    objtealfrost.style.visibility="hidden";
+
+    objchocake.style.position='relative';
+    objchocake.style.left='-440px';
+    objchocake.style.top='0px';
+    objchocake.style.visibility="hidden";
+
+    objvanillacake.style.position='relative';
+    objvanillacake.style.left='-530px';
+    objvanillacake.style.top='-10px';
+    objvanillacake.style.visibility="hidden";
+
+    objroll.style.position='relative';
+    objroll.style.left='-610px';
+    objroll.style.top='-10px';
+    objroll.style.visibility="hidden";
+
+    objeclair.style.position='relative';
+    objeclair.style.left='-810px';
+    objeclair.style.top='-10px';
+    objeclair.style.visibility="hidden";
+
+    objstars.style.position='relative';
+    objstars.style.left='-90px';
+    objstars.style.top='5px';
+    objstars.style.visibility="hidden"
+
+    objcherries.style.position='relative';
+    objcherries.style.left='20px';
+    objcherries.style.top='0px';
+    objcherries.style.visibility="hidden"
+
+    Objmacaron.style.position='relative';
+    Objmacaron.style.left='-890px';
+    Objmacaron.style.top='0px';
+    Objmacaron.style.visibility="hidden"
+
+    objbonbon.style.position='relative';
+    objbonbon.style.left='-710px';
+    objbonbon.style.top='0px';
+    objbonbon.style.visibility="hidden"
+
+
+
+}
